bc76b1a221a0ce0298640c66fc2c0c505644d6a3
Chethan Gowda <chethanrgowda38@gmail.com>
2022-07-28 21:06:29 +0530

    Add project files.

---
+++ TrainerTrainee/Trainee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainerTrainee
+{
+    internal class Trainee
+    {
+        public Trainer Trainer { get; set; }
+        public List<Training> trainings { get; set; } = new List<Training>();
+    }
+}
---
+++ TrainerTrainee/Training.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainerTrainee
+{
+    internal class Training
+    {
+        public Trainer trainer { get; set; }
+        public List<Trainee> trainees { get; set; } = new List<Trainee>();
+        public Course course { get; set; }
+
+        public int No_of_trainees()
+        {
+            return trainees.Count;
+        }
+
+        public string Organization_name()
+        {
+            return trainer.org.Name;
+        }
+
+        public int Training_duration()
+        {
+            int duration = 0;
+            foreach(Module module in course.Modules )
+            {
+                foreach (Unit unit in module.units)
+                {
+                    duration += unit.Duration;
+                }
+            }
+            return duration;
+        }
+
+
+    }
+}
